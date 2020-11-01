8459a539e9d05eac379999f6180c21e2e832fb0a
DiegoFleitas <diego.fleitas68@gmail.com>
2020-11-01 15:38:05 -0300

    secrets exmaple

---
+++ src/resources/secrets.example.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego
+ * Date: 1/13/2019
+ * Time: 11:41 PM
+ */
+
+namespace BotStatusBot\src\Bot\Classes;
+
+// Facebook App
+// Got mine from Paintmin's tool https://maxman013.github.io/token/?fbclid=IwAR1KXWrvLwBf6eaflyvpkyzsNfCDNU8PbDvLMekSsxG5p-1OXBvjJOPxTn4
+$_APP_ID = '';
+$_APP_SECRET = '';
+$_CLIENT_TOKEN = '';
+$_PAGE_ID = '';
+$_ACCESS_TOKEN_DEBUG = '';
+
+// Added your cx or search engine ID here, see https://cse.google.com/cse/
+$_GOOGLE_SEARCHENGINE_ID = '';
+$_GOOGLE_APPNAME = '';
+$_GOOGLE_DEVKEY = '';
+
+// Wikia BOT login
+$_LGTOKEN = "";
+$_LGNAME = "";
+$_LGPASSWORD = "";
+$_EDIT_TOKEN= "";
